1c827f3b4b3826bfe9a243c7d8aa4cbe043c183b
Lucas Gazzola <lucasgazzola1@outlook.com>
2022-06-23 03:42:29 -0300

    make it better

--- index.ts
+++ index.ts
@@ -316,7 +316,7 @@ async function getAllCategories(){
 }
 
 app.get('/api/categories', async(req: express.Request, res: express.Response) => {
-  const categories = getAllCategories() ;
+  const categories = await getAllCategories() ;
   console.log(categories);
   res.status(200).json(categories);
 });
