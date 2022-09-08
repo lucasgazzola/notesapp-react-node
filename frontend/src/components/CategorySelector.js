4a4669b7796c55875dd2a3ed4cade84f31289866
Lucas Gazzola <lucasgazzola1@outlook.com>
2022-09-08 07:34:57 -0300

    add react-select

--- frontend/src/components/CategorySelector.js
+++ frontend/src/components/CategorySelector.js
@@ -1,77 +1,73 @@
-import './CategorySelector.css';
-import { getAllCategories, getNotesOfCategory } from '../services/categoriesService';
+import Select from 'react-select'
+import './CategorySelector.css'
+import {
+  getAllCategories,
+  getNotesOfCategory,
+} from '../services/categoriesService'
 import { getAllNotes } from '../services/notesService'
-import { useEffect, useState } from 'react';
-import { useContext } from 'react';
-import { NotesContext } from '../context/NotesContext';
-import { AppContext } from '../context/AppContext';
-import { useLocation } from 'react-router-dom';
-
+import { useEffect, useState } from 'react'
+import { useContext } from 'react'
+import { NotesContext } from '../context/NotesContext'
+import { AppContext } from '../context/AppContext'
+import { useLocation } from 'react-router-dom'
 
 export default function CategorySelector() {
-  const [categories, setCategories] = useState([]);
-
-  const [selectedOption, setSelectedOption] = useState('alloptiondefault');
-
-  const currentLocation = useLocation();
-  const isArchived = currentLocation.pathname.includes('archived');
-
-  const { setNotes, notes } = useContext(NotesContext);
-  const { reFetch } = useContext(AppContext);
+  const [categories, setCategories] = useState([])
+  const [selectedOption, setSelectedOption] = useState('alloptiondefault')
+  const [options, setOptions] = useState([])
+  const currentLocation = useLocation()
+  const isArchived = currentLocation.pathname.includes('archived')
+  const { setNotes, notes } = useContext(NotesContext)
+  const { reFetch } = useContext(AppContext)
 
   useEffect(() => {
-    setSelectedOption('alloptiondefault');
+    setSelectedOption('alloptiondefault')
   }, [currentLocation])
 
   useEffect(() => {
-    getAllCategories()
-      .then(setCategories)
-      .catch(console.error)
+    getAllCategories().then(setCategories).catch(console.error)
   }, [reFetch, isArchived, notes])
 
-  const handleSelectChange = async (e) => {
-    const categoryName = e.target.value;
-    setSelectedOption(categoryName);
+  useEffect(() => {
+    setOptions([
+      { value: 'alloptiondefault', label: 'All' },
+      ...categories.map(({ name }) => ({
+        value: name,
+        // First letter to UpperCase
+        label: name.charAt(0).toUpperCase() + name.slice(1),
+      })),
+    ])
+  }, [categories])
+
+  const handleSelectChange = async e => {
+    const categoryName = e?.value
+    setSelectedOption(categoryName)
     if (categoryName === 'alloptiondefault') {
       await getAllNotes()
-        .then(notes => setNotes(notes?.filter(note => note.isArchived === isArchived)))
+        .then(notes =>
+          setNotes(notes?.filter(note => note.isArchived === isArchived))
+        )
         .catch(console.error)
     } else {
       await getNotesOfCategory(categoryName)
-        .then(notes => setNotes(notes?.filter(note => note.isArchived === isArchived)))
+        .then(notes =>
+          setNotes(notes?.filter(note => note.isArchived === isArchived))
+        )
         .catch(console.error)
     }
   }
 
   return (
-    <div className='CategorySelector--container'>
-      <select
-        className='CategorySelector'
-        name='categoryselector'
-        placeholder='Select a category'
-        title='Select a category'
-        value={selectedOption}
-        onChange={handleSelectChange}
-      >
-        <option
-          key='alloptiondefault'
-          value='alloptiondefault'
-          defaultValue={true}
-        >
-          All
-        </option>
-        {
-          categories?.map(({ name }) => (
-            <option
-              className='CategorySelector--option'
-              key={name}
-              value={name}
-            >
-              {name}
-            </option>
-          ))
-        }
-      </select>
-    </div>
+    <>
+      {/* //TODO: implement multiselection */}
+      {options && (
+        <Select
+          onChange={handleSelectChange}
+          defaultValue={selectedOption}
+          options={options}
+          className="CategorySelector"
+        />
+      )}
+    </>
   )
 }
